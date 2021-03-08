cea9f57ab5ff39208b8b46c0687c6bd9a73c936a
hannah26hannah <ooyoonoo24@gmail.com>
2021-03-09 03:31:37 +0900

    :fire: removed useless redux files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import {
   Box,
-  Grommet
+  Grommet,
+  ResponsiveContext
 } from 'grommet';
+import { useDispatch } from 'react-redux';
+import { changeSize } from './store/actions'
 
 import myMedia from './components/common/social.js';
 import Header from './components/common/Header.js';
@@ -10,18 +13,33 @@ import Body from './components/common/Body.js';
 import Footer from './components/common/Footer.js';
 import theme from './components/common/theme.js'
 
+
 const MainBody = () => (
   <Body channel={myMedia} />
 )
+
+
 function App() {
-  return (
-    <Grommet id='container' theme={theme}>
-      <Box>
-        <Header channel={myMedia} theme={theme}/>
-      </Box>
-      <MainBody />
-      <Footer channel={myMedia} />
-    </Grommet>
+  const dispatch = useDispatch();
+  
+  return (  
+      <Grommet id='container' theme={theme}>
+        <ResponsiveContext.Consumer>{
+          size => { 
+            dispatch(changeSize(size))
+            return (
+              <>
+                <Box>
+                  <Header channel={myMedia} theme={theme} />
+                </Box>
+                <MainBody />
+                <Footer channel={myMedia} />
+              </>
+            )
+          } 
+        }
+        </ResponsiveContext.Consumer>
+      </Grommet>
   );
 }
 
--- src/store/index.js
+++
@@ -1,3 +0,0 @@
-import configure from './config';
-
-export default configure();
