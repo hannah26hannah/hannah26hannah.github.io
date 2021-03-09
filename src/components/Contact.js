8bee9eef213469f1ea5e2a670127b8da2a7e4cdd
hannah26hannah <ooyoonoo24@gmail.com>
2021-03-09 21:46:57 +0900

    :iphone: change alignment in Contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,45 +1,36 @@
-import { Box, List, Text, Anchor } from 'grommet';
+import { Box, Text, Anchor, Grid, Grommet } from 'grommet';
 import React from 'react';
 import { useSelector } from 'react-redux'
 import { withTranslation } from 'react-i18next';
+import theme from './common/theme.js'
 
 function Contact (props) {
-    const channel = useSelector(state => state.channel.channel);
+    const channels = useSelector(state => state.channel.channel);
     const {t} = props
 
     return (
-        <List
-            data={channel}
-            pad='small'
-            border={false}
-            direction='column'
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'start'
-            }}>
-            {datum => (
-            <Box 
-                direction='row-responsive' flex gap='medium' alignSelf='center' size='medium' align='center' justify='around' pad='small'>
-                <Anchor 
-                    icon={datum.icon} 
-                    href={datum.href} 
-                    a11yTitle={t(datum.a11yTitle)} 
-                    key={datum.name} size='small'
-                    style={{
-                        backgroundColor: 'orange',
-                        width: '3rem',
-                        height: '3rem',
-                        borderRadius: '1rem',
-                        fontSize: '2rem'
-                        }}
-                />
-                <Text weight='normal' size='medium'>
-                    {t(datum.a11yTitle)}
-                </Text>
-            </Box>)}
-        </List>
+        <Grommet theme={theme} style={{ display: 'flex', justifyContent: 'center'}}>
+            <Grid columns={['small', 'small']} rows={['small', 'small']} gap='small'>
+                {channels.map(channel => (
+                <Box flex  justify='center' align='center' gap='medium'>
+                    <Anchor 
+                        icon={channel.icon}
+                        href={channel.href} 
+                        a11yTitle={t(channel.a11yTitle)} 
+                        key={channel.name} size='xlarge'
+                        style={{
+                            backgroundColor: 'orange',
+                            width: '3rem', height: '3rem',
+                            borderRadius: '1rem',}}/>
+                    <Text weight='normal' size='medium'>
+                        {t(channel.a11yTitle)}
+                    </Text>
+                </Box>))}
+            </Grid>
+        </Grommet>
+        
     )
 }
+
 
 export default withTranslation()(Contact);
